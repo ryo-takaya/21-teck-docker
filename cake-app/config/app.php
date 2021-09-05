741c220aea5fe2647ef98db4836f2e4d02988fff
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-09-05 15:19:43 +0900

    デバッグモードをtrueにする

--- cake-app/config/app.php
+++ cake-app/config/app.php
@@ -274,9 +274,9 @@ return [
             /*
              * It is recommended to set these options through your environment or app_local.php
              */
-            //'username' => 'my_app',
-            //'password' => 'secret',
-            //'database' => 'my_app',
+            'username' => 'user',
+            'password' => 'password',
+            'database' => 'master',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
